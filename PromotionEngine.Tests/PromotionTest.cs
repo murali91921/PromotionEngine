f24580174e6075f73c1d0939b787f9808f2ccf4a
Murali <murali.isu@gmail.com>
2020-09-14 18:23:55 +0530

    New Test project added.

---
+++ PromotionEngine.Tests/PromotionTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngine.Tests
+{
+    [TestClass]
+    public class PromotionTest
+    {
+        [TestMethod]
+        public void TestOrder_1()
+        {
+        }
+    }
+}
